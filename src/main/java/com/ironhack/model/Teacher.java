0b1ef015f5b5ab53d32d21fdc1cdab171f9b7fb3
Ali Safarli <alisafarli0606@gmail.com>
2026-03-06 15:49:35 +0400

    feat: Removed unnecessary spaces.And adding "get id".

--- src/main/java/com/ironhack/model/Teacher.java
+++ src/main/java/com/ironhack/model/Teacher.java
@@ -5,15 +5,18 @@ import java.util.UUID;
 public class Teacher {
 
     private final String id;
-
     private String name;
     private double salary;
 
 
-    public Teacher( String name, double salary) {
+    public Teacher(String name, double salary) {
         this.id = UUID.randomUUID().toString();
         this.name = name;
         this.salary = salary;
+    }
+
+    public String getId() {
+        return id;
     }
 
     public String getName() {
