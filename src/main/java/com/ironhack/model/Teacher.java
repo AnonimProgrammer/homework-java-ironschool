a3491bf9086c7d46729ed51ec99f7dcefa79f61b
Ali Safarli <alisafarli0606@gmail.com>
2026-03-06 15:42:00 +0400

    feat: Teacher model is added.

---
+++ src/main/java/com/ironhack/model/Teacher.java
@@ -0,0 +1,34 @@
+package com.ironhack.model;
+
+import java.util.UUID;
+
+public class Teacher {
+
+    private final String id;
+
+    private String name;
+    private double salary;
+
+
+    public Teacher( String name, double salary) {
+        this.id = UUID.randomUUID().toString();
+        this.name = name;
+        this.salary = salary;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
